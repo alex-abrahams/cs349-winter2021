60f97414ef797ae81c9c3fa05c4983dea6151fad
alex abrahams <aj2abrah@uwaterloo.ca>
2021-04-07 00:54:56 -0400

    basis for A4

---
+++ A4/app/src/main/java/ca/uwaterloo/cs349/SharedViewModel.java
@@ -0,0 +1,19 @@
+package ca.uwaterloo.cs349;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SharedViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SharedViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is shared model");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
